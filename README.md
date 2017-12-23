f0197e7a078b202197bbe2320e7677ce4e3a0cde
Wicem KERKENI <kerkeni.w@decade.fr>
2017-12-23 03:56:56 +0100

    Default EC2 instance

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# terraform-samples
+#Terraform Samples
+
+This repo contains all terraform scripts used in all my leaning cursus on Terraform and posted on my Blog https://www.kerkeni.net
---
+++ sample1-ec2/main.tf
@@ -0,0 +1,31 @@
+# Configure the AWS Provider
+# Paris data center not handled yet by terraform
+
+provider "aws" {
+  region     = "eu-west-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "HelloWorld"
+  }
+}
